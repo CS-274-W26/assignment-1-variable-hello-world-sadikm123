1568aa2702fabb33ae09c861f869654b509e9af3
Mohamed Sadik <sadikm@oregonstate.edu>
2026-01-16 19:39:44 -0800

    Completed assignment 1

--- prog1.c
+++ prog1.c
@@ -1,4 +1,6 @@
 #include <stdio.h> // Necessary for printf()
+#include <stdlib.h> // Necessary for srand() and rand()
+#include <time.h> // Necessary for time()
 
 // TODO Other includes
 
@@ -8,4 +10,17 @@ int main() {
 	// is executed, it will automatically start here.
 	
 	// TODO Complete the program
+	
+	srand(time(NULL)); // Seed the random number generator
+	int myrand = rand() % 10; // synthesize our preferred random number range
+
+	printf("The rando number was: %d\n", myrand);
+
+	if (myrand >= 0 && myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if (myrand >= 5 && myrand <= 9) {
+		printf("FRODO LIVES\n");
+	} else if (myrand == 10) {
+		printf("Larn is the best roguelike\n");
+	}
 }
